a110e76527390dd2564e9776cc398555b74bdedf
vector-y <vt.victortran@gmail.com>
2021-06-14 15:15:15 -0700

    prepare for mdx

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,38 @@
 import { ChakraProvider, ColorModeProvider, useColorMode, ThemeProvider, theme, CSSReset } from "@chakra-ui/react";
 import customTheme from '../styles/theme'
 import '../styles/globals.css'
+import { Global, css } from '@emotion/react'
+
+const GlobalStyle = ({ children }) => {
+  const { colorMode } = useColorMode()
+  return (
+    <>
+      <Global
+        styles={css`
+          ::selection {
+            background-color: #90CDF4;
+            color: #fefefe;
+          }
+          ::-moz-selection {
+            background: #ffb7b7;
+            color: #fefefe;
+          }
+          html {
+            min-width: 356px;
+            scroll-behavior: smooth;
+          }
+          #__next {
+            display: flex;
+            flex-direction: column;
+            min-height: 100vh;
+            background: ${colorMode === 'light' ? 'white' : '#171717'};
+          }
+        `}
+      />
+      {children}
+    </>
+  )
+}
 
 function MyApp({ Component, pageProps }) {
   return(
@@ -14,7 +46,9 @@ function MyApp({ Component, pageProps }) {
           useSystemColorMode: true,
         }}
       >
-        <Component {...pageProps} />
+        <GlobalStyle>
+          <Component {...pageProps} />
+        </GlobalStyle>
       </ColorModeProvider>
     </ChakraProvider>
   ); 
--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,49 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Container from '../components/Container'
-import { Heading, Link, Text, Code, Flex, Box } from '@chakra-ui/layout'
+import { Link, Code, Box } from '@chakra-ui/layout'
+import {
+  useColorMode,
+  Heading,
+  Text,
+  Flex,
+  Stack
+} from '@chakra-ui/react'
 
 export default function Index() {
+  const {colorMode} = useColorMode()
+  const colorSecondary = {
+    light: 'gray.700',
+    dark: 'gray.400'
+  }
   return (
     <Container>
-
+      <Head>
+        <title> Home | Victor Tran </title>
+      </Head>
+      <Stack
+         as="main"
+         spacing={8}
+         justifyContent="center"
+         alignItems="flex-start"
+         m="0 auto 4rem auto"
+         maxWidth="700px"
+         px={2}
+      >
+        <Flex
+          flexDirection="column"
+          justifyContent="flex-start"
+          alignItems="flex-start"
+          maxWidth="700px"
+        >
+          <Heading mb={2}>
+            Hey, I'm Victor
+          </Heading>
+          <Text color={colorSecondary[colorMode]}>
+            How are you?
+          </Text>
+        </Flex>
+      </Stack>
     </Container>
   )
 }
