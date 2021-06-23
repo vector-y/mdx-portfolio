1c7783792052bc72376ac6ea1adcd335396500a8
vector-y <vt.victortran@gmail.com>
2021-06-23 15:26:37 -0700

    footer added

---
+++ components/Footer.tsx
@@ -0,0 +1,77 @@
+import {
+    Box,
+    chakra,
+    Container,
+    Stack,
+    Text,
+    useColorModeValue,
+    VisuallyHidden,
+  } from '@chakra-ui/react';
+  import { FaMediumM, FaTwitter, FaYoutube } from 'react-icons/fa';
+  import { ReactNode } from 'react';
+  
+  const SocialButton = ({
+    children,
+    label,
+    href,
+  }: {
+    children: ReactNode;
+    label: string;
+    href: string;
+  }) => {
+    return (
+      <chakra.button
+        bg={useColorModeValue('blackAlpha.100', 'whiteAlpha.100')}
+        rounded={'full'}
+        w={8}
+        h={8}
+        cursor={'pointer'}
+        as={'a'}
+        href={href}
+        display={'inline-flex'}
+        alignItems={'center'}
+        justifyContent={'center'}
+        transition={'background 0.3s ease'}
+        _hover={{
+          bg: useColorModeValue('blackAlpha.200', 'whiteAlpha.200'),
+        }}>
+        <VisuallyHidden>{label}</VisuallyHidden>
+        {children}
+      </chakra.button>
+    );
+  };
+  
+  export default function SmallWithSocial() {
+    return (
+      <Box
+        paddingTop={'10%'}
+        bg={useColorModeValue('white', 'gray.900')}
+        color={useColorModeValue('gray.700', 'gray.200')}>
+        <Container
+          as={Stack}
+          maxW={'80%'}
+          paddingLeft={'20%'}
+          py={4}
+          direction={{ base: 'column', md: 'row' }}
+          spacing={4}
+          justify={{ base: 'center', md: 'space-between' }}
+          alignItems={{ base: 'center', md: 'center' }}
+          position={'absolute'}
+          bottom={0}
+          >
+          <Text>© 2021 victor tran.</Text>
+          <Stack direction={'row'} spacing={6}>
+            <SocialButton label={'Twitter'} href={'https://twitter.com/vectori'}>
+              <FaTwitter />
+            </SocialButton>
+            <SocialButton label={'YouTube'} href={'https://www.youtube.com/channel/UC6_piwoyrqG1f_PF5CzK-hw/videos'}>
+              <FaYoutube />
+            </SocialButton>
+            <SocialButton label={'Medium'} href={'https://medium.com/@vt.victortran'}>
+              <FaMediumM />
+            </SocialButton>
+          </Stack>
+        </Container>
+      </Box>
+    );
+  }
---
+++ pages/about.js
@@ -0,0 +1,49 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+import Container from '../components/Container'
+import { Link, Code, Box } from '@chakra-ui/layout'
+import Hero from '../components/Hero'
+import Grid from '../components/Grid.tsx'
+import Footer from  '../components/Footer.tsx'
+import {
+  useColorMode,
+  Heading,
+  Text,
+  Flex,
+  Stack
+} from '@chakra-ui/react'
+
+export default function Index() {
+  const {colorMode} = useColorMode()
+  const colorSecondary = {
+    light: 'gray.700',
+    dark: 'gray.400'
+  }
+  return (
+    <Container>
+      <Head>
+        <title> Home | Victor Tran </title>
+      </Head>
+      <Stack
+         as="main"
+         spacing={8}
+         justifyContent="center"
+         alignItems="flex-start"
+         m="0 auto 4rem auto"
+         maxWidth="700px"
+         px={2}
+      >
+        <Flex
+          flexDirection="column"
+          justifyContent="flex-start"
+          alignItems="flex-start"
+          maxWidth="700px"
+        >
+          <Hero />
+        </Flex>
+      </Stack>
+      <Footer />
+    </Container>
+  )
+}
