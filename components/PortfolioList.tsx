772dc2c613b36edd64761b8a3d5268521010f86a
vector-y <vt.victortran@gmail.com>
2021-11-14 17:46:35 -0800

    removed ikigai from homepage

--- components/PortfolioList.tsx
+++ components/PortfolioList.tsx
@@ -72,7 +72,80 @@ const ArticleList = () => {
                     Featured Work
                 </Text>
             </Heading>
-        <Box
+
+            <Box
+            marginTop={{ base: '1', sm: '5' }}
+            display="flex"
+            flexDirection={{ base: 'column', sm: 'row' }}
+            justifyContent="space-between">
+            <Box
+            display="flex"
+            flex="2"
+            marginRight="3"
+            position="relative"
+            alignItems="center">
+                <Box
+                    width={{ base: '100%', sm: '85%' }}
+                    zIndex="2"
+                    marginLeft={{ base: '0', sm: '5%' }}
+                    marginTop="5%">
+                    <Link textDecoration="none" _hover={{ textDecoration: 'none' }}>
+                        <NextLink href={"/portfolio/botan"} passHref>
+                            <Image
+                            alt={'botan'}
+                            borderRadius="lg"
+                            objectFit={'contain'}
+                            src={
+                                '/BotanGradient.png'
+                            }
+                            />
+                        </NextLink>
+                    </Link>
+                </Box>
+                <Box zIndex="1" width="100%" position="absolute" height="100%">
+                    <Box
+                    bgGradient={useColorModeValue(
+                        'radial(orange.600 1px, transparent 1px)',
+                        'radial(orange.300 1px, transparent 1px)'
+                    )}
+                    backgroundSize="20px 20px"
+                    opacity="0.4"
+                    height="100%"
+                    />
+                </Box>
+            </Box>
+            <Box
+            display="flex"
+            flex="3"
+            flexDirection="column"
+            justifyContent="center"
+            marginTop={{ base: '3', sm: '0' }}>
+            <Heading marginTop="1" marginBottom="2">
+                <Link textDecoration="none" _hover={{ textDecoration: 'none' }}>
+                    <NextLink href={"/portfolio/botan"} passHref>
+                        botan
+                    </NextLink>
+                </Link>
+                <Text
+                as="p"
+                marginTop="2"
+                color={useColorModeValue('gray.700', 'gray.200')}
+                fontSize="lg">
+                September 2021 - November 2021
+                </Text>
+            </Heading>
+            <BlogTags tags={['Environment', 'Community', 'Cal Arts']} />
+            <Text
+                as="p"
+                marginTop="2"
+                color={useColorModeValue('gray.700', 'gray.200')}
+                fontSize="lg">
+                An in-person rooftop community gardening service that aims to reconnect the community through hands-on agricultural education, events, and volunteering.
+            </Text>
+            </Box>
+        </Box>
+
+        {/* <Box
             marginTop={{ base: '1', sm: '5' }}
             display="flex"
             flexDirection={{ base: 'column', sm: 'row' }}
@@ -142,7 +215,7 @@ const ArticleList = () => {
                 ikigai is a MVP web application which combines e-commerce and sustainability (enables thrifting to combat fast fashion).
             </Text>
             </Box>
-        </Box>
+        </Box> */}
 
 
 
@@ -157,35 +230,35 @@ const ArticleList = () => {
             marginRight="3"
             position="relative"
             alignItems="center">
-            <Box
-                width={{ base: '100%', sm: '85%' }}
-                zIndex="2"
-                marginLeft={{ base: '0', sm: '5%' }}
-                marginTop="5%">
-                <Link textDecoration="none" _hover={{ textDecoration: 'none' }}>
-                    <NextLink href={"/portfolio/coingo"} passHref>
-                        <Image
-                        alt={'coingo'}
-                        borderRadius="lg"
-                        objectFit={'contain'}
-                        src={
-                            '/coingo.png'
-                        }
-                        />
-                    </NextLink>
-                </Link>
-            </Box>
-            <Box zIndex="1" width="100%" position="absolute" height="100%">
                 <Box
-                bgGradient={useColorModeValue(
-                    'radial(orange.600 1px, transparent 1px)',
-                    'radial(orange.300 1px, transparent 1px)'
-                )}
-                backgroundSize="20px 20px"
-                opacity="0.4"
-                height="100%"
-                />
-            </Box>
+                    width={{ base: '100%', sm: '85%' }}
+                    zIndex="2"
+                    marginLeft={{ base: '0', sm: '5%' }}
+                    marginTop="5%">
+                    <Link textDecoration="none" _hover={{ textDecoration: 'none' }}>
+                        <NextLink href={"/portfolio/coingo"} passHref>
+                            <Image
+                            alt={'coingo'}
+                            borderRadius="lg"
+                            objectFit={'contain'}
+                            src={
+                                '/coingo.png'
+                            }
+                            />
+                        </NextLink>
+                    </Link>
+                </Box>
+                <Box zIndex="1" width="100%" position="absolute" height="100%">
+                    <Box
+                    bgGradient={useColorModeValue(
+                        'radial(orange.600 1px, transparent 1px)',
+                        'radial(orange.300 1px, transparent 1px)'
+                    )}
+                    backgroundSize="20px 20px"
+                    opacity="0.4"
+                    height="100%"
+                    />
+                </Box>
             </Box>
             <Box
             display="flex"
